aecce408ddf103d278b3a4a3c1828476665a438e
iamroot69 <150319344+iamroot69@users.noreply.github.com>
2024-05-19 17:42:36 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dm_practicals",
+    version="0.1",
+    description="A sample Python package",
+    author="Root",
+    author_email="rokvin99341@gmail.com",
+    packages=["my_package"],
+    install_requires=[],
+)
